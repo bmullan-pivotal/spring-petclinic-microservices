09adaf94289ceffd44b36c3769bfc3ada3dafff4
Oded Shopen <odedia@gmail.com>
2020-05-11 10:31:42 +0300

    Removed unneeded comments

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/DiscoveryConfig.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/DiscoveryConfig.java
@@ -2,9 +2,7 @@ package org.springframework.samples.petclinic.customers;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-//@Profile("!cloud")
 @Configuration
 @EnableDiscoveryClient
 public class DiscoveryConfig {
